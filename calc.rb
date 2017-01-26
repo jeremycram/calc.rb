82fba59d02e811f02073336d1236095aa60b9572
Jeremy Cram <lungburner@gmail.com>
2017-01-25 19:54:08 -0700

    first commit

---
+++ calc.rb
@@ -0,0 +1,56 @@
+def main_menu
+  puts 'choose function [add],[multiply],[subtract],[devide],[exit program]'
+response = gets.chomp
+  if response == "add" then
+  add
+elsif response == "subtract" then
+    subtract
+  elsif response == "multiply" then
+    multiply
+  elsif response == "exit program" then
+    exit_program
+  elsif response == "devide"
+    devide
+  end
+end
+
+def add
+  puts 'which numbers would you like to add?'
+  n1 = gets.chomp.to_i
+  n2 = gets.chomp.to_i
+  puts (n1 + n2)
+
+end
+
+def multiply
+  puts 'which numbers would you like to multiply?'
+  n1 = gets.chomp.to_i
+  n2 = gets.chomp.to_i
+  puts (n1 * n2)
+
+end
+
+def subtract
+  puts 'which numbers would you like to subtract?'
+  n1 = gets.chomp.to_i
+  n2 = gets.chomp.to_i
+  puts (n2 - n1)
+
+end
+
+def devide
+  puts 'which numbers would you like to devide?'
+  n1 = gets.chomp.to_i
+  n2 = gets.chomp.to_i
+  puts (n1 % n2)
+
+end
+
+def exit_program
+  puts "goodbye"
+  exit(0)
+end
+
+while true do
+ main_menu
+end
